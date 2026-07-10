9aedd33677173aaa015c7e890baa4202ea41b929
Manas koshti <koshtimanas@gmail.com>
2026-07-10 09:16:24 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,1 +1,10 @@
+echo "Welcome to the Jenkins Hell😊"
+echo "User : $(whoami)"
+echo "Hostname : $(hostname)"
+echo "Current Directory : $(pwd)"
+echo "Date : $(date)"
 
+echo "Installing Update"
+sudo apt update
+sudo apt install -y nginx
+nginx -version
